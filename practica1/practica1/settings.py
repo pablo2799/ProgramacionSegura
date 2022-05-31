c2632058279fb5a114ffb28840de74e3e1b964ab
pablo2799 <jp_rl@hotmail.com>
2022-05-30 23:03:46 -0500

    Practica de manejo de configuracion sensible hecha

--- practica1/practica1/settings.py
+++ practica1/practica1/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qbp4%spff3cj^wb!s^ub47yun9ecde#+$jj(y&1cn!19d17&pr'
-
+SECRET_KEY = os.environ.get('LLAVEDJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'practica1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistemaEvaluacion',
+        'USER': 'usuario1',
+        'PASSWORD': os.environ.get('PASSUSRBD'),
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
